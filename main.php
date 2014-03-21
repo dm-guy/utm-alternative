897fa86d55585a3098248cb68f0557dff4e975d2
dm-guy <yanwest@gmail.com>
2014-03-21 11:02:04 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,19 +1,20 @@
 <?php
-/* check whether there is a campaign cookie set */
+/* check whether there is a campaign cookie that was set before */
 if (isset($_COOKIE["campaign_cookie"]))
 	{
-		$campaign_variable = $_COOKIE['campaign_cookie']; 
+		$campaign_variable = $_COOKIE['campaign_cookie']; // see HTML below how to use this variable
 	}
 
 /* If there is no cookie, create one */ 
 elseif (isset($_GET['utm_campaign'])) {
-	$campaign_name = htmlspecialchars($_GET['utm_campaign'], ENT_QUOTES);
+	$campaign_name = htmlspecialchars($_GET['utm_campaign']);
 	$expire = time()+60*60*24*30*24; //two years
 	setcookie('campaign_cookie', $campaign_name, $expire);
 	$campaign_variable = $_COOKIE['campaign_cookie'];
-	Header('Location: '.$_SERVER['PHP_SELF']);
+	Header('Location: ' . $_SERVER['PHP_SELF']);
 }
  
+
 ?>
 
 
