a348ac7e3f2647f4fdd96e8a2a027ea83bf7fc27
dm-guy <yanwest@gmail.com>
2014-03-24 16:21:37 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,13 +1,13 @@
+<!-- how to use $campaign_variable in your forms -->
 
 <?php 
-require_once (your-file.php)
+require_once (choose_your_approach.php)
 ?>
 <!DOCTYPE html>
 <html>
 
 	<body>		
 		<form>
-			<!-- how to use $campaign_variable in your forms -->
 			<input type="hidden" id="lead_source" name="lead_source" value="<?php echo $campaign_variable; ?>" />
 		</form>
 	</body>
